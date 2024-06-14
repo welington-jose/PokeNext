c64147c91c175f42f4cd50c203f3ce91e35ab4b7
welington-jose <welingtonjose.dev@gmail.com>
2024-06-13 20:26:55 -0400

    css

--- src/app/card/page.js
+++ src/app/card/page.js
@@ -1,16 +1,22 @@
 import Image from "next/image"
+import Link from "next/link"
+import styles from "@/app/styles/Card.module.css"
 
 export default function Card({pokemon}) {
 
     return(
-        <div>
+        <div className={styles.card}>
             <Image
             src={`https://raw.githubusercontent.com/wellrccity/pokedex-html-js/master/assets/img/pokemons/poke_${pokemon.id}.gif`}
             width={120}
-            height={160}
+            height={170}
             alt={pokemon.name}
             />
-            <h3>{pokemon.name}</h3>
+            <p className={styles.id}>#{pokemon.id}</p>
+            <h3 className={styles.title}>{pokemon.name}</h3>
+            <Link className={styles.btn} href={`/pokemon/${pokemon.id}`}>
+                Detalhes
+            </Link>
         </div>
     )
 }
